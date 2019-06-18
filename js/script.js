94ef918e5d6a4cdbeb2c3375c4f3182ada240d13
Danny Chai <chaiduo123@163.com>
2019-06-18 22:19:32 +0800

    a little addition to my page

--- js/script.js
+++ js/script.js
@@ -7,17 +7,48 @@
 
 function on_contact_click(){
 
-	document.getElementById('button_home').className -= 'active';
+	document.getElementById('button_home').className -= ' active';
 	document.getElementById('button_contact').className += ' active';
+	document.getElementById('button_about').className -= ' active'
 	// let tmp = $.ajax(url:'../snippet/myleetcode.html');
-	// $('#main-code').load('./snippet/myleetcode.txt');
-	document.getElementById('main-code').style.display = 'none';
+	// $('#about').load('./snippet/myleetcode.txt');
+	document.getElementById('about').style.display = 'none';
 	document.getElementById('contact').style.display = 'inherit';
+	document.getElementById('home').style.display = 'none';
 }
 
-var element = document.getElementById('button_contact');
+function on_about_click(){
 
-element.onclick = on_contact_click;
+	document.getElementById('button_home').className -= ' active';
+	document.getElementById('button_contact').className -= ' active';
+	document.getElementById('button_about').className += ' active'
+	// let tmp = $.ajax(url:'../snippet/myleetcode.html');
+	// $('#about').load('./snippet/myleetcode.txt');
+	document.getElementById('about').style.display = 'inherit';
+	document.getElementById('contact').style.display = 'none';
+	document.getElementById('home').style.display = 'none';
+}
+
+function on_home_click(){
+
+	document.getElementById('button_home').className += ' active';
+	document.getElementById('button_contact').className -= ' active';
+	document.getElementById('button_about').className -= ' active'
+	// let tmp = $.ajax(url:'../snippet/myleetcode.html');
+	// $('#about').load('./snippet/myleetcode.txt');
+	document.getElementById('about').style.display = 'none';
+	document.getElementById('contact').style.display = 'none';
+	document.getElementById('home').style.display = 'inherit';
+}
+
+var home = document.getElementById('button_home');
+home.onclick = on_home_click;
+
+var contack = document.getElementById('button_contact');
+contack.onclick = on_contact_click;
+
+var about = document.getElementById('button_about');
+about.onclick = on_about_click;
 
 $('.navbar-collapse a').click(function(){
 	$('.navbar-collapse').collapse('hide');
