c3c13e2c2d60813a5e09f8db058071215c056d93
Danny Chai <chaiduo123@163.com>
2019-08-03 23:49:21 +0800

    a little addition to my page

--- js/script.js
+++ js/script.js
@@ -7,42 +7,42 @@
 
 function on_contact_click(){
 
-	document.getElementById('button_home').className -= ' active';
+	// document.getElementById('button_home').className -= ' active';
 	document.getElementById('button_contact').className += ' active';
 	document.getElementById('button_about').className -= ' active'
 	// let tmp = $.ajax(url:'../snippet/myleetcode.html');
 	// $('#about').load('./snippet/myleetcode.txt');
 	document.getElementById('about').style.display = 'none';
 	document.getElementById('contact').style.display = 'inherit';
-	document.getElementById('home').style.display = 'none';
+	// document.getElementById('home').style.display = 'none';
 }
 
 function on_about_click(){
 
-	document.getElementById('button_home').className -= ' active';
+	// document.getElementById('button_home').className -= ' active';
 	document.getElementById('button_contact').className -= ' active';
 	document.getElementById('button_about').className += ' active'
 	// let tmp = $.ajax(url:'../snippet/myleetcode.html');
 	// $('#about').load('./snippet/myleetcode.txt');
 	document.getElementById('about').style.display = 'inherit';
 	document.getElementById('contact').style.display = 'none';
-	document.getElementById('home').style.display = 'none';
+	// document.getElementById('home').style.display = 'none';
 }
 
-function on_home_click(){
+// function on_home_click(){
 
-	document.getElementById('button_home').className += ' active';
-	document.getElementById('button_contact').className -= ' active';
-	document.getElementById('button_about').className -= ' active'
-	// let tmp = $.ajax(url:'../snippet/myleetcode.html');
-	// $('#about').load('./snippet/myleetcode.txt');
-	document.getElementById('about').style.display = 'none';
-	document.getElementById('contact').style.display = 'none';
-	document.getElementById('home').style.display = 'inherit';
-}
+// 	document.getElementById('button_home').className += ' active';
+// 	document.getElementById('button_contact').className -= ' active';
+// 	document.getElementById('button_about').className -= ' active'
+// 	// let tmp = $.ajax(url:'../snippet/myleetcode.html');
+// 	// $('#about').load('./snippet/myleetcode.txt');
+// 	document.getElementById('about').style.display = 'none';
+// 	document.getElementById('contact').style.display = 'none';
+// 	document.getElementById('home').style.display = 'inherit';
+// }
 
-var home = document.getElementById('button_home');
-home.onclick = on_home_click;
+// var home = document.getElementById('button_home');
+// home.onclick = on_home_click;
 
 var contack = document.getElementById('button_contact');
 contack.onclick = on_contact_click;
